7c5bd699cf1202ccf5baacea77982061b7a5a4e5
Victoria Liao 品真 <victorialiao1994@gmail.com>
2022-02-23 16:20:32 +0000

    init

--- src/main/scala/example/Lists.scala
+++ src/main/scala/example/Lists.scala
@@ -22,7 +22,9 @@ object Lists:
    * @param xs A list of natural numbers
    * @return The sum of all elements in `xs`
    */
-  def sum(xs: List[Int]): Int = ???
+  def sum(xs: List[Int]): Int = {
+    xs.sum
+  }
 
   /**
    * This method returns the largest element in a list of integers. If the
@@ -37,4 +39,6 @@ object Lists:
    * @return The largest element in `xs`
    * @throws java.util.NoSuchElementException if `xs` is an empty list
    */
-  def max(xs: List[Int]): Int = ???
+  def max(xs: List[Int]): Int = {
+    xs.max
+  }
